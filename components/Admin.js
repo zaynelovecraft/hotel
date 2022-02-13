96b5136ccaf748e1a36f7b8257994d0a7479c5c7
Meow Security <meowsecurity@Meows-MacBook-Pro.local>
2022-02-12 18:40:37 -0800

    new

--- components/Admin.js
+++ components/Admin.js
@@ -47,9 +47,9 @@ const [declined, setDeclined] = useState(false)
          <section>
              <div className=' pb-2 border-b'>
                 <div className='flex justify-center'>
-                    <h1 onClick={()=>{setResopen(!resopen), closeall('res')}} className={`mr-4 relative  text-gray-700  cursor-pointer ${resopen === true && 'text-cyan-500'} hover:text-cyan-500 text-sm`}>Reservations {resopen === false && (<span className='absolute text-red-500 text-[10px] animate-pulse font-bold -top-1 '>2</span>)} </h1>
+                    <h1 onClick={()=>{setResopen(!resopen), closeall('res')}} className={`mr-4 relative  text-gray-700  cursor-pointer ${resopen === true && 'text-cyan-500'} hover:text-cyan-500 text-sm`}>Reservations {resopen === false && (<span className='absolute text-red-500 text-[10px] animate-pulse font-bold -top-1 -right-2 '>2</span>)} </h1>
                     <h1 onClick={()=>{setUseropen(!useropen), closeall('user')}} className='mr-4 cursor-pointer  text-gray-700  hover:text-cyan-500 text-sm'>Users</h1>
-                    <h1 className='text-sm relative cursor-pointer text-gray-700 hover:text-cyan-500'>Messages <span className='absolute text-red-500 text-[10px]  animate-pulse font-bold -top-1 '>99+</span></h1>
+                    <h1 className='text-sm relative cursor-pointer text-gray-700 hover:text-cyan-500'>Messages <span className='absolute text-red-500 text-[10px] -right-2 animate-pulse font-bold -top-1 '>99+</span></h1>
                 </div>
              </div>
          </section>
@@ -66,7 +66,7 @@ const [declined, setDeclined] = useState(false)
                 
 <div className='flex mt-2 mb-2 justify-center'>
 
-                 <div><h1 onClick={()=>{setPending(!pending),closeall('pen')}} className={`mr-4 cursor-pointer ${pending === true && 'text-cyan-500'}  relative hover:text-cyan-500 text-[12px]`}>Pending <span className='absolute text-red-500 text-[10px] animate-pulse font-bold -top-1 '>2</span> </h1></div>
+                 <div><h1 onClick={()=>{setPending(!pending),closeall('pen')}} className={`mr-4 cursor-pointer ${pending === true && 'text-cyan-500'}  relative hover:text-cyan-500 text-[12px]`}>Pending <span className='absolute text-red-500 -right-2 text-[10px] animate-pulse font-bold -top-1 '>2</span> </h1></div>
                  <div><h1 onClick={()=> {setApproved(!approved), closeall('app')}} className={`mr-4  cursor-pointer hover:text-cyan-500 text-[12px] ${approved === true && 'text-cyan-500'}`}>Approved</h1></div>
                  <div><h1 onClick={()=>{setDeclined(!declined), closeall('dec')}} className=' text-[12px] cursor-pointer  hover:text-cyan-500'>Declined</h1></div>
 </div>
