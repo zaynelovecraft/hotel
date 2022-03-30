842b975a6ff0600b8ffec3c94d7e93b93eea4b0d
Meow Security <meowsecurity@Meows-MacBook-Pro.local>
2022-03-29 20:24:33 -0700

    fix

--- components/SupportWindow/Supportwindow.js
+++ components/SupportWindow/Supportwindow.js
@@ -12,7 +12,7 @@ function Supportwindow({visible}) {
     <div>
       
       <div className={`transition ease-in-out fixed right-[17px] z-20 ${visible === true ? `fixed ` : `hidden`} opacity-100 rounded-lg bottom-[111px] w-[245px] sm:w-[350px] sm:h-[500px] lg:w-[415px] lg:h-[560px] h-[450px] shadow-lg bg-cyan-200 duration-300`} ></div>
-    <div className={`transition ease-in-out fixed bottom-[116px] ${visible === true ? `fixed ` : `hidden`} opacity-100 z-30 right-[24px] w-[232px] sm:w-[335px] sm:h-[489px] lg:w-[400px] lg:h-[550px] h-[439px] bg-white rounded-lg overflow-hidden duration-300`} >
+    <div className={`transition ease-in-out fixed bottom-[116px] ${visible === true ? `fixed ` : `hidden`} opacity-100 z-50 right-[24px] w-[232px] sm:w-[335px] sm:h-[489px] lg:w-[400px] lg:h-[550px] h-[439px] bg-white rounded-lg overflow-hidden duration-300`} >
       <Emailform 
         setChatuser={chatuser => setChatuser(chatuser)}
         setChat={chat => setChat(chat)}
