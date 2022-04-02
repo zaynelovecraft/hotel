2bd521a1ec524cbb7eed687f864d3d6383dd2d27
Meow Security <meowsecurity@Meows-MacBook-Pro.local>
2022-04-01 17:45:45 -0700

    added alert to message tab

--- components/ChatEngine/Avatar.js
+++ components/ChatEngine/Avatar.js
@@ -1,7 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { supabase } from "../../utils/supabase-client";
+import { useUser } from "../../utils/useUser";
+import { AiFillBell } from "@react-icons/all-files/ai/AiFillBell";
 
 function Avatar(props) {
+
+  const [data, setData] = useState(false);
   const [hovered, setHovered] = useState(false)
+  const { signUp, user, signIn } = useUser();
+  const [show, setShow] = useState(false);
+
+console.log(data)
+
+const getalert = async() => {
+
+  const { data, error } = await supabase
+  .from('Messages')
+  .select('read')
+  .match({user_id: user?.id})
+  if(data) {
+    setData(data[0]?.read)
+  }
+}
+useEffect(() => {
+  getalert()
+}, [user])
+
+
   return (
     <div  className="z-30">
       <div className={`transition ease-in-out bg-white py-2 shadow-lg ${hovered === true ? `opacity-100 bg-opacity-90` : `opacity-0`} fixed bottom-[37px] lg:bottom-[25px] border-cyan-500 border right-[65px] sm:right-[120px] z-30  px-5 rounded-full duration-300`}>
@@ -10,12 +35,15 @@ function Avatar(props) {
         </h2>
       </div>
       <div
-        onClick={() => props.onClick && props.onClick()}
+        onClick={() => {props.onClick && props.onClick(), getalert()}}
         onMouseEnter={() => setHovered(true)}
         onMouseLeave={() => setHovered(false)}
         className={`fixed z-30 transition ease-in ${hovered === true ? `border-yellow-300`: `border-cyan-500 `} shadow-lg border-[4px] cursor-pointer bottom-2 right-1 sm:right-4 sm:bottom-4 rounded-full hover:border-[1px] hover:border-yellow-300 sm:w-[84px] sm:h-[84px]  w-[60px] h-[60px]`}
-      >
+      > 
         <img className="rounded-full bg-cover" src="/chat.webp" />
+        {
+          data === true && <div className="absolute -top-[11px] text-[25px] text-pink-500 font-bold  left-[1px]"><AiFillBell/></div>
+        }
       </div>
     </div>
   );
